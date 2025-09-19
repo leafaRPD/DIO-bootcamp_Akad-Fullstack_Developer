72cd35dca6f1e943089fba34f5a4922f1752f3de
leafaRPD <rafaelpd@gmail.com>
2025-09-19 13:15:43 -0300

    gravando no git

--- Primeiro exemplo .NET/Program.cs
+++ Primeiro exemplo .NET/Program.cs
@@ -95,3 +95,59 @@ calc.Somar(3, 4); //vai chamar o método Somar da classe Calculadora, que imprim
 calc.Potencia(3, 2);
 calc.Seno(30);
 
+
+
+// exemplo de Do While:
+int soma = 0, numero = 0;
+
+do
+{
+    Console.WriteLine("Digite um número (0 -> encerra o programa)");
+    numero = Convert.ToInt32(Console.ReadLine());
+soma += numero;
+}   while (numero != 0) ;
+
+Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+
+// Construindo um menu interativo
+
+string opcao;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.WriteLine("Digite uma das opções:");
+    Console.WriteLine("1 - Cadastro cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+
+opcao = Console.ReadLine();
+
+switch (opcao)
+    {
+    case "1":
+            Console.WriteLine("Cadastro cliente");
+            break;
+
+    case "2":
+            Console.WriteLine("Buscar cliente");
+            break;
+
+    case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+    case "4":
+            Console.WriteLine("Encerrar");
+            //Environment.Exit(0); // o programa se encerra e, se mais códigos forem implementados após isso, não rodará. Então o mais corretor é trocar por:
+            exibirMenu = false; // assim ele torna o while 'false', o que faz com que encerre o while sem encerrar o programa inteiro.
+            break;
+
+    default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
